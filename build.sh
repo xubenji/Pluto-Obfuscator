b1de26303345be37b533a2ec0c3b4fbf3ce78851
Benji Xu <459547070@qq.com>
2024-02-24 15:00:38 -0500

    Update build.sh
    support web assembly

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd build
 
 if [ "$(uname)" == "Darwin" ]; then
     cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" \
-	-DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
+        -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
         -DCMAKE_BUILD_TYPE=Release \
         -DDEFAULT_SYSROOT=$(xcrun --show-sdk-path) \
         -DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX$(xcrun --show-sdk-version).sdk \
@@ -17,8 +17,8 @@ if [ "$(uname)" == "Darwin" ]; then
         ../llvm
 else   
     cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" \
-	-DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
-        -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" \
+        -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
+        -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86;WebAssembly" \
         -DBUILD_SHARED_LIBS=On -DCMAKE_INSTALL_PREFIX="../install" ../llvm
 fi
 
@@ -27,3 +27,33 @@ echo "-- Building with $processors CPU cores"
 
 ninja -j$processors
 ninja install
+
+# processors=$1
+# if [ -z $processors ]; then
+#     processors=$(nproc)
+# fi
+
+# mkdir -p build
+# cd build
+
+# if [ "$(uname)" == "Darwin" ]; then
+#     cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" \
+# 	-DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
+#         -DCMAKE_BUILD_TYPE=Release \
+#         -DDEFAULT_SYSROOT=$(xcrun --show-sdk-path) \
+#         -DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX$(xcrun --show-sdk-version).sdk \
+#         -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" \
+#         -DCMAKE_INSTALL_PREFIX="../install" \
+#         ../llvm
+# else   
+#     cmake -G "Ninja" -DLLVM_ENABLE_PROJECTS="clang" \
+# 	-DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ \
+#         -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="X86" \
+#         -DBUILD_SHARED_LIBS=On -DCMAKE_INSTALL_PREFIX="../install" ../llvm
+# fi
+
+# echo "-- Building on $(uname)"
+# echo "-- Building with $processors CPU cores"
+
+# ninja -j$processors
+# ninja install
